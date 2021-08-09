75ac0f1962322968722995d0def2fd73ba7e7311
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-08-09 18:07:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -17,7 +23,12 @@ function modelLoaded()
 
 function draw()
 {
-    backgroud('#969A97');
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of a Square will be = " + difference + "px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results)
@@ -25,5 +36,14 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX + " noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        differnece = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + differnece);
     }
 }
